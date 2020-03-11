94ff547fcf4efd53341f57981537b0d1cd99a62b
Afif Fadhlurrahman <49097125+afiffadhlurrahman@users.noreply.github.com>
2020-03-11 16:11:55 +0000

    Create soal_3a.c

---
+++ soal3/soal_3a.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <wait.h>
+#include <stdio.h>
+
+int main() {
+  pid_t child_id;
+  int status;
+  child_id = fork();
+  
+  if (child_id < 0) {
+    exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
+
+  }
+
+  if (child_id == 0) {
+    char *argv[] = {"mkdir", "/home/afif/modul2/indomie", NULL};
+    execv("/bin/mkdir", argv);
+  } 
+  else {
+    // this is parent
+    while ((wait(&status)) > 0);
+    char *argv[] = {"mkdir", "/home/afif/modul2/sedap", NULL};
+    execv("/bin/mkdir", argv);
+  }
+}

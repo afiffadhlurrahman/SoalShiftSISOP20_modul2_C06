8ec5bcc10bd60b2f0c0d6a5e15cb839c25e4fb68
Afif Fadhlurrahman <49097125+afiffadhlurrahman@users.noreply.github.com>
2020-03-13 09:08:12 +0000

    Update soal_2b.c

--- soal2/soal_2b.c
+++ soal2/soal_2b.c
@@ -41,84 +41,84 @@ int main() {
     close(STDERR_FILENO);
 
     while (1) {
-        pid_t child_id,child_id1;
-        int status;    
+        pid_t child_id;
+        int status;  
+
+        char buffer[80];
+    	char b[]="/home/afif/sisop/shift2/";
+        time_t rawtime;
+	    struct tm *info;
+        time( &rawtime );
+        info = localtime( &rawtime );
+        strftime(buffer,80,"%Y-%m-%d_%H:%M:%S", info);
+        strcat(b,buffer);  	      
+
         child_id = fork();
-        child_id1 = fork();
-
-        if (child_id == 0) {
-            char b[]="/home/afif/sisop/shift2/";
-            char c[1000];
-            struct tm ti;
-            time_t t_of_day;
-            time_t s, val = 1;
-            struct tm* current_time;
-            
-            s = time(NULL);
-            current_time = localtime(&s);
-            
-            ti.tm_year = current_time->tm_year;     // Year - 1900
-            ti.tm_mon = current_time->tm_mon;       // Month, where 0 = jan
-            ti.tm_mday = current_time->tm_mday;     // Day of the month
-            ti.tm_hour = current_time->tm_hour;
-            ti.tm_min = current_time->tm_min;
-            ti.tm_sec = current_time->tm_sec;
-            ti.tm_isdst = -1;        // Is DST on? 1 = yes, 0 = no, -1 = unknown
-            t_of_day = mktime(&ti);
-
-            sprintf( c, "%ld", (long) t_of_day );
-            strcat(b,c);
-            sleep(25);
-            char *binaryPath = "/bin/mkdir";
-            char *argv[] = {binaryPath, b, NULL};
-            execv(binaryPath, argv);
-            
+        if (child_id < 0) {
+            exit(EXIT_FAILURE);            
         }
-        if(child_id1 == 0){
-            struct tm t;
-            time_t t_of_day;
-            time_t s, val = 1;
+      
+        if (child_id == 0) {
+            if(fork()==0){
+                char *binaryPath = "/bin/mkdir";
+                char *argv[] = {binaryPath, b, NULL};
+                execv(binaryPath, argv);
+            }
+            else{
+                while ((wait(&status)) > 0);
+                int count=20;
+                while(count>0){
+                    if (fork() == 0)
+                    {
+                        chdir(b);
+                        
+                        struct tm t;
+                        time_t t_of_day;
+                        time_t s, val = 1;
 
-            struct tm* current_time;
-            s = time(NULL);
-            current_time = localtime(&s);
-            
-            t.tm_year = current_time->tm_year;  // Year - 1900
-            t.tm_mon = current_time->tm_mon;           // Month, where 0 = jan
-            t.tm_mday = current_time->tm_mday;          // Day of the month
-            t.tm_hour = current_time->tm_hour;
-            t.tm_min = current_time->tm_min;
-            t.tm_sec = current_time->tm_sec;
-            t.tm_isdst = -1;        // Is DST on? 1 = yes, 0 = no, -1 = unknown
-            t_of_day = mktime(&t);
-            
-            t_of_day = (t_of_day%1000)+100;
-            char destination[1000]="https://picsum.photos/";
-            char buffer[1000];
-            
-            sprintf(buffer,"%ld",(long)t_of_day);
-            strcat(destination,buffer);
-            
-            time_t rawtime;
-            struct tm *info;
-            char name[80];
-            char namefile[80];
+                        struct tm* current_time;
+                        s = time(NULL);
+                        current_time = localtime(&s);
+                        
+                        t.tm_year = current_time->tm_year;          // Year - 1900
+                        t.tm_mon = current_time->tm_mon;            // Month, where 0 = jan
+                        t.tm_mday = current_time->tm_mday;          // Day of the month
+                        t.tm_hour = current_time->tm_hour;
+                        t.tm_min = current_time->tm_min;
+                        t.tm_sec = current_time->tm_sec;
+                        t.tm_isdst = -1;                    // Is DST on? 1 = yes, 0 = no, -1 = unknown
+                        t_of_day = mktime(&t);
+                        
+                        t_of_day = (t_of_day%1000)+100;
+                        char destination[1000]="https://picsum.photos/";
+                        char buffer[1000];
+                        
+                        sprintf(buffer,"%ld",(long)t_of_day);
+                        strcat(destination,buffer);
+                        
+                        time_t rawtime;
+                        struct tm *info;
+                        char name[80];
+                        char namefile[80];
 
-            time( &rawtime );
-            info = localtime( &rawtime );
+                        time( &rawtime );
+                        info = localtime( &rawtime );
 
-            char wher[100];
-            sprintf(wher,"%s",getcwd(wher,100));
+                        strftime(name,80,"%Y-%m-%d_%I:%M:%S", info);
+                        strcat(namefile,name);
 
-            strftime(name,80,"%Y-%m-%d_%I:%M:%S", info);
-            strcat(namefile,name);
-            char *binaryPath1 = "/usr/bin/wget";
-            char *argv[] = {binaryPath1,"-P",wher,"O",namefile, destination, NULL};
-            execv(binaryPath1, argv);
+                        char *binaryPath1 = "/usr/bin/wget";
+                        char *argv[] = {binaryPath1,"-P",b,"-qO",namefile, destination, NULL};
+                        execv(binaryPath1, argv);
+                    }
+                    count--;
+                    sleep(5);   
+                }
+                    //zip
+            }
         }
         else{
-            while ((wait(&status)) > 0);
+            sleep(30);
         }            
-        sleep(5);
     }
 }
